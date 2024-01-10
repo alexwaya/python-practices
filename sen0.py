181273aae2056031027bd4f85b45bab24cd9128a
Alexander Waya Nguku <alex.waya@gmail.com>
2024-01-10 22:28:31 +0300

    Create a program that takes a user's name and age as input and prints a greeting message

--- sen0.py
+++ sen0.py
@@ -1,24 +1,36 @@
 # START - Calculate the sum of two numbers entered by the user
 
-# a = int(input("Enter a: "))
-# b = int(input("Enter b: "))
+    # a = int(input("Enter a: "))
+    # b = int(input("Enter b: "))
 
-# def SumTotal():
-#     sum = a + b   
-#     return sum
-# print(SumTotal())
+    # def SumTotal():
+    #     sum = a + b   
+    #     return sum
+    # print(SumTotal())
 
 # STOP - Calculate the sum of two numbers entered by the user
 
 
 # START - Write a Python program to calculate the area of a rectangle given its length and width
-def calculate_area_of_rectangle():
-    length = int(input("Enter length of the rectangle: "))
-    width = int(input("Enter width of the rectangle: "))
-    area = length * width
-    return area
-print(calculate_area_of_rectangle())
-
-
+    # def calculate_area_of_rectangle():
+    #     length = int(input("Enter length of the rectangle: "))
+    #     width = int(input("Enter width of the rectangle: "))
+    #     area = length * width
+    #     return area
+    # print(calculate_area_of_rectangle())
 # STOP - Write a Python program to calculate the area of a rectangle given its length and width
+
+
+# START - Create a program that takes a user's name and age as input and prints a greeting message
+def greeting():
+    name = str(input("Enter your first name: "))
+    age = int(input("Enter your age: "))
+    great = "Howdy, " + name + "!" + " Good to see that you are " + str(age) + "."
+    return great
+print(greeting())
+
+
+# STOP - Create a program that takes a user's name and age as input and prints a greeting message
+
+
 
