4014cb0ec186caad109c642225adf6191af40602
Alexander Waya Nguku <alex.waya@gmail.com>
2024-01-10 22:18:17 +0300

    calculate area of a rectangle

--- sen0.py
+++ sen0.py
@@ -1,45 +1,24 @@
-# import re
+# START - Calculate the sum of two numbers entered by the user
 
-# def StringChallenge(sen):
-#     # Remove punctuation from the string
-#     sen = re.sub(r'[^\w\s]', '', sen)
-    
-#     # Split the string into words
-#     longest_string = sen.split()
-    
-#     # Find the longest word
-#     StringChallenge = max(longest_string, key=len)
-    
-#     # Concatenate the longest word with 'ChallengeToken'
-#     sen = StringChallenge + 'ChallengeToken'
-    
-#     # Replace every third character with 'X'
-#     sen = ''.join([c if (i+1) % 3 != 0 else 'X' for i, c in enumerate(sen)])
-    
-#     return sen
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
 
-# print(StringChallenge("fun&!! time"))
-
-
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-
-def SumTotal():
-
-    sum = a + b   
-    return sum
- 
-print(SumTotal())
-
-
-
- 
-
-# def add_two_numbers():
-#     num1 = float(input("Enter the first number: "))
-#     num2 = float(input("Enter the second number: "))
-#     sum = num1 + num2
+# def SumTotal():
+#     sum = a + b   
 #     return sum
+# print(SumTotal())
 
-# # Call the function
-# print(add_two_numbers())
+# STOP - Calculate the sum of two numbers entered by the user
+
+
+# START - Write a Python program to calculate the area of a rectangle given its length and width
+def calculate_area_of_rectangle():
+    length = int(input("Enter length of the rectangle: "))
+    width = int(input("Enter width of the rectangle: "))
+    area = length * width
+    return area
+print(calculate_area_of_rectangle())
+
+
+# STOP - Write a Python program to calculate the area of a rectangle given its length and width
+
